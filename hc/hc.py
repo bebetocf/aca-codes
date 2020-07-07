79bd980fc7280ead453adef5e15d7fe472e30ca2
Roberto <betocostaf@gmail.com>
2020-07-07 09:36:09 -0300

    Organizing code

--- hc/hc.py
+++ hc/hc.py
@@ -1,11 +1,36 @@
 import tsplib95, random, copy
 
+# TODO: trocar duas cidades aleatorias
+# Filho nao pode ficar muito diferente do pai
+# No comeco pode trocar muitos filhos e depois trocar pouco
+
 def f_n(nodes, points, n_nodes):
     dist = 0
     for i in range(n_nodes-1):
         edge = nodes[i], nodes[i+1]
         dist += points.get_weight(*edge)
     return dist
+
+def swap_position(nodes, i, j):
+    nodes[i], nodes[j] = nodes[j], nodes[i]
+
+def swap_close_cities(nodes, points, n_nodes):
+    best_value = f_n(nodes, points, n_nodes)
+    best_path = copy.deepcopy(nodes)
+    best_att = False
+
+    for i in range(n_nodes-1):
+        swap_position(nodes, i, i+1)
+        value = f_n(nodes, points, n_nodes)
+        
+        if value < best_value:
+            best_value = value
+            best_path = copy.deepcopy(nodes)
+            best_att = True
+
+        swap_position(nodes, i, i+1)
+
+    return best_value, best_path, best_att
 
 def find_best_solution(points, n_nodes):
     nodes = list(points.get_nodes())
@@ -15,18 +40,8 @@ def find_best_solution(points, n_nodes):
 
     while best_att:
         nodes = copy.deepcopy(best_path)
-        best_att = False
-
-        for i in range(n_nodes-1):
-            nodes[i], nodes[i+1] = nodes[i+1], nodes[i]
-            value = f_n(nodes, points, n_nodes)
-            
-            if value < best_value:
-                best_value = value
-                best_path = copy.deepcopy(nodes)
-                best_att = True
-
-            nodes[i], nodes[i+1] = nodes[i+1], nodes[i]
+        
+        best_value, best_path, best_att = swap_close_cities(nodes, points, n_nodes)
 
         n_iterations += 1
 

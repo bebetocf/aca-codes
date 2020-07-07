00d3cbfe16de493d8c7243434a27d64bd890618d
Roberto <betocostaf@gmail.com>
2020-07-07 11:15:35 -0300

    Writing solutions in a file

--- hc/hc.py
+++ hc/hc.py
@@ -1,8 +1,9 @@
 import tsplib95, random, copy
 
-n_tries = 100
-n_random_swap = 50
+n_tries = 10
+n_random_swap = 100
 n_child_random_swap = 1
+swap_strategy = 'close'
 
 # TODO: trocar duas cidades aleatorias
 # Filho nao pode ficar muito diferente do pai
@@ -79,8 +80,10 @@ def find_best_solution(points, n_nodes):
     while best_att:
         nodes = copy.deepcopy(best_path)
 
-        # best_value, best_path, best_att = swap_close_cities(nodes, points, n_nodes)
-        best_value, best_path, best_att = swap_random_cities(nodes, points, n_nodes, n_random_swap, n_child_random_swap)
+        if swap_strategy == 'close': 
+            best_value, best_path, best_att = swap_close_cities(nodes, points, n_nodes)
+        elif swap_strategy == 'random':
+            best_value, best_path, best_att = swap_random_cities(nodes, points, n_nodes, n_random_swap, n_child_random_swap)
 
         # print ("\t[", best_value, "]:", best_path)
 
@@ -88,26 +91,46 @@ def find_best_solution(points, n_nodes):
 
     return best_path, best_value, n_iterations
 
+def write_list_file(fl, lst):
+    fl.write('[')
+    for j in range(len(lst)):
+        fl.write(str(lst[j]) + (', ' if (j != (len(lst) - 1)) else ']\n'))
+
+
 def main():
-    points_path = "./dj38.tsp"
+    points_path = "dj38.tsp"
     points = tsplib95.load(points_path)
     n_nodes = len(list(points.get_nodes()))
-    all_value = float('inf')
+    all_value = 1000000000
+
+    file_name = points_path.split('.')[0] + '_' + str(n_tries) + '_' + swap_strategy
+    if swap_strategy == 'random':
+        file_name += '_' + str(n_random_swap) + '_' + str(n_child_random_swap)
+    file_name += '.txt'
+    file_log = open('results/log_' + file_name, 'w')
 
     for i in range(n_tries):
         best_path, best_value, best_n = find_best_solution(points, n_nodes)
         if best_value < all_value:
-            all_path, all_value, all_n, all_try = best_path, best_value, best_n, i
+            all_path, all_value, all_n, all_try = best_path, best_value, best_n, (i+1)
 
-        # print ("[", (i+1), "]:")
-        # print ("\tDistância do melhor caminho:", best_value)
-        # print ("\tIterações executadas:", best_n)
-        # print ("\tMelhor caminho:", best_path)
-            
-    print ("Overall best[", all_try , "]:")
-    print ("\tDistância do melhor caminho:", best_value)
-    print ("\tIterações executadas:", best_n)
-    print ("\tMelhor caminho:", best_path)
+        file_log.write("[" + str(i+1) + "]:" + "\n")
+        file_log.write("\tDistância do melhor caminho: " + str(best_value) + "\n")
+        file_log.write("\tIterações executadas: " + str(best_n) + "\n")
+        file_log.write("\tMelhor caminho: ")
+        write_list_file(file_log, best_path)
+
+    file_log.close()
+
+    file_best = open('results/best_' + file_name, 'w')
+
+    file_best.write("Overall best[" + str(all_try)  + "]:" + "\n")
+    file_best.write("\tDistância do melhor caminho: " + str(all_value) + "\n")
+    file_best.write("\tIterações executadas: " + str(all_n) + "\n")
+    file_best.write("\tMelhor caminho: ")
+    write_list_file(file_best, all_path)
+
+    file_best.close()
 
 if __name__ == '__main__':
     main()
